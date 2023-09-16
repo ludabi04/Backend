e7d7c7165e84f7c9cb63a5f7632b5058553bb6c5
Lucas <ludabi@live.com>
2023-09-16 18:11:06 -0300

    renderizando productos en el home

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { ProductManagerFiles } from "./persistence/productManagerFiles.js";
-console.log(ProductManagerFiles)
 import { productRouter } from "./routes/productos.routes.js";
 import { cartsRouter } from "./routes/cart.routes.js";
+import { __dirname } from "./utils.js";
+import path from "path";
+import { engine } from "express-handlebars"
+import { viewsRouter } from "./routes/views.routes.js";
+
 
 const managerProductService = new ProductManagerFiles("./src/files/productos.json")
 console.log(managerProductService)
@@ -13,7 +17,22 @@ const app = express();
 app.use(express.urlencoded({extended:true})) // permite caracteres especiales
 app.use(express.json());
 
+
 app.listen(port, () => console.log("server funcionando"))
+
+app.use(express.static(path.join(__dirname, "/public")))
+
 
 app.use("/api/products", productRouter);
 app.use("/api/carts", cartsRouter);
+
+// configuracion del motor de plantillas 
+
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, "views"));
+
+
+
+
+app.use(viewsRouter)
---
+++ src/routes/views.routes.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import { productsService } from "../persistence/index.js";
+
+
+const router = Router();
+
+
+//routes 
+
+router.get("/", async (req, res) => {
+    try {
+        const products = await productsService.getProducts();
+        const data = { productos: products }
+        res.render("home", data)
+    } catch (error) {
+        
+    }
+    
+
+});
+
+
+
+export {router as viewsRouter}
