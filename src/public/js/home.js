220c113eaf12f438f7e7bf32331ffd037fd230d3
Lucas <ludabi@live.com>
2023-10-05 20:38:04 -0300

    fin

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -6,19 +6,20 @@ socketClient.on("productosActualizados", (data) => {
     let prodElem = "";
     data.forEach(elm => {
         prodElem +=
-        `<div class="card" style="width: 18rem; border: 2px solid">
-            <img src=${elm.thumbnail} class="card-img-top" alt="...">
-        <div class="card-body">
-        <p>ID: #${elm._id}</p>
-        <h5 class="card-title" id="title">${elm.title}</h5>
-        <p class="card-text">Disponibles: ${elm.stock}</p>
-        <p class="card-text">Precio: $ ${elm.price}</p>
-        <p class="card-text">Categoria: ${elm.category}</p>
-        <button onclick=eliminar("${elm._id}")>Eliminar</button> 
-        <button onclick=addCart("${elm._id}")>Agregar al carrito</button>
-        
-</div> 
-</div>`
+        `<div class="homeProd">
+                <div class="card homeProd" style="width: 18rem; border: 2px solid">
+                    <img src=${elm.thumbnail} class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p>ID: #${elm._id}</p>
+                        <h5 class="card-title" id="title">${elm.title}</h5>
+                        <p class="card-text">Disponibles: ${elm.stock}</p>
+                        <p class="card-text">Precio: $ ${elm.price}</p>
+                        <p class="card-text">Categoria: ${elm.category}</p>
+                        <button onclick=eliminar("${elm._id}")>Eliminar</button> 
+                        <button onclick=addCart("${elm._id}")>Agregar al carrito</button>
+                    </div> 
+                </div>
+        </div>`
 });
     productList.innerHTML = prodElem;
 });
