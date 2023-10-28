9088d86107f2c40815c9c14c3837473106780033
Lucas <ludabi@live.com>
2023-10-28 02:42:12 -0300

    css

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,19 @@
 import path from "path";
 import { fileURLToPath } from "url";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const __dirname = path.dirname(fileURLToPath(import.meta.url));
 console.log("__dirname", __dirname) //Users/lucasbianco/Desktop/Coder/BACKEND/Backend/Backend/src
 
 //__dirname va a ser la ruta de la carpeta donde esta la carpeta
 
+const PRIVATE_KEY = "ldb";
+
+export const generateToken = (user) => {
+    const token = jwt.sign({ name: user.name, email: user.email }, PRIVATE_KEY, { expiresIn: "24h" })
+    return token;
+}
 export const createHash = (password) => {
     return bcrypt.hashSync(password, bcrypt.genSaltSync())
 };
